a71d6a89c1dc946e8c54a11e01e8723a4676bb9e
dimitrii <dmitry.zyadik@gmail.com>
2026-03-19 16:10:59 +0500

    0319 1610 41-50 lvl

--- lib/ver_2/services/storage_service.dart
+++ lib/ver_2/services/storage_service.dart
@@ -21,7 +21,8 @@ class StorageService {
   static Future<Set<int>> getCompletedLevels() async {
     final prefs = await _prefs;
     //TODO: Верную в зад после теста
-    const completedString = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40'; // = prefs.getString(_completedLevelsKey) ?? '';
+    const completedString =
+        '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61'; // = prefs.getString(_completedLevelsKey) ?? '';
     if (completedString.isEmpty) return <int>{};
     return completedString.split(',').map(int.parse).toSet();
   }
